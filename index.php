1084760ec9828c02dd014e882c60f6e435aa1ec3
Victory Odumeh <vodumeh@gmail.com>
2022-12-12 10:37:22 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+
+
+?>
+
+<!Doctype HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" description="device-width=1.0">
+    <title>Online Shopping Portal || Register</title>
+</head>
+<body></body>
+</html>
---
+++ register.php
@@ -0,0 +1,26 @@
+<?php
+
+
+
+
+
+?>
+
+<!Doctype HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rush Shopping Portal || Registration</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">Rush</div>
+    </nav>
+
+    <!-- Registration Form -->
+    <section class="register" id="register">
+
+    </section>
+</body>
+</html>
