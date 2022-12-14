5b9e5a6398161a0ce59a28904ca84293dbdbeb9e
Victory Odumeh <vodumeh@gmail.com>
2022-12-14 22:46:45 +0100

    saved

--- register.php
+++ register.php
@@ -12,6 +12,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Rush Shopping Portal || Registration</title>
+    <link rel="stylesheet" href="styles/style.css"
 </head>
 <body>
     <nav>
