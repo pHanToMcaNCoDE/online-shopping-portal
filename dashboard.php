b89d26716d7cc32e6961e9745a3b9c887da7e0a4
Victory Odumeh <vodumeh@gmail.com>
2022-12-15 21:23:22 +0100

    saved

---
+++ dashboard.php
@@ -0,0 +1,45 @@
+<?php
+
+
+
+
+
+?>
+
+<!Doctype HTML>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" description="device-width=1.0">
+    <title>Online Shopping Portal || Register</title>
+    <link rel="stylesheet" href="styles/dash.css">
+</head>
+<body>
+    <aside class="nav">
+        <div class="logo">
+            <a href="#">Rush</a>
+        </div>
+        <div class="links">
+            <div>
+                <a href="#">Dashboard</a>
+                </hr>
+            </div>
+            <div>
+                <a href="#">Products</a>
+                </hr>
+            </div>
+            <div>
+                <a href="#">Orders</a>
+                </hr>
+            </div>
+            <div>
+                <a href="#">Customers</a>
+                </hr>
+            </div>
+        </div>
+        <div class="acc">
+            <a href="#">Vick</a>
+        </div>
+</aside>
+</body>
+</html>
